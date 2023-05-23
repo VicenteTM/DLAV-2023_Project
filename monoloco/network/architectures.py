0655887c33300781ff2ac642071ec748a7454ef7
maxdargent <maxime.dargent@epfl.ch>
2023-05-23 03:38:40 +0200

    Added transformer layer
    Co-authored-by: Vicente <VicenteTM@users.noreply.github.com>

--- monoloco/network/architectures.py
+++ monoloco/network/architectures.py
@@ -1,105 +1,8 @@
 
 import torch
 from torch import nn
-
-
-class LocoModel(nn.Module):
-
-    def __init__(self, input_size, output_size=2, linear_size=512, p_dropout=0.2, num_stage=3, device='cuda'):
-        super().__init__()
-
-        self.num_stage = num_stage
-        self.stereo_size = input_size
-        self.mono_size = int(input_size / 2)
-        self.output_size = output_size - 1
-        self.linear_size = linear_size
-        self.p_dropout = p_dropout
-        self.num_stage = num_stage
-        self.linear_stages = []
-        self.device = device
-
-        # Initialize weights
-
-        # Preprocessing
-        self.w1 = nn.Linear(self.stereo_size, self.linear_size)
-        self.batch_norm1 = nn.BatchNorm1d(self.linear_size)
-
-        # Internal loop
-        for _ in range(num_stage):
-            self.linear_stages.append(MyLinearSimple(self.linear_size, self.p_dropout))
-        self.linear_stages = nn.ModuleList(self.linear_stages)
-
-        # Post processing
-        self.w2 = nn.Linear(self.linear_size, self.linear_size)
-        self.w3 = nn.Linear(self.linear_size, self.linear_size)
-        self.batch_norm3 = nn.BatchNorm1d(self.linear_size)
-
-        # ------------------------Other----------------------------------------------
-        # Auxiliary
-        self.w_aux = nn.Linear(self.linear_size, 1)
-
-        # Final
-        self.w_fin = nn.Linear(self.linear_size, self.output_size)
-
-        # NO-weight operations
-        self.relu = nn.ReLU(inplace=True)
-        self.dropout = nn.Dropout(self.p_dropout)
-
-    def forward(self, x):
-
-        y = self.w1(x)
-        y = self.batch_norm1(y)
-        y = self.relu(y)
-        y = self.dropout(y)
-
-        for i in range(self.num_stage):
-            y = self.linear_stages[i](y)
-
-        # Auxiliary task
-        y = self.w2(y)
-        aux = self.w_aux(y)
-
-        # Final layers
-        y = self.w3(y)
-        y = self.batch_norm3(y)
-        y = self.relu(y)
-        y = self.dropout(y)
-        y = self.w_fin(y)
-
-        # Cat with auxiliary task
-        y = torch.cat((y, aux), dim=1)
-        return y
-
-
-class MyLinearSimple(nn.Module):
-    def __init__(self, linear_size, p_dropout=0.5):
-        super().__init__()
-        self.l_size = linear_size
-
-        self.relu = nn.ReLU(inplace=True)
-        self.dropout = nn.Dropout(p_dropout)
-
-        self.w1 = nn.Linear(self.l_size, self.l_size)
-        self.batch_norm1 = nn.BatchNorm1d(self.l_size)
-
-        self.w2 = nn.Linear(self.l_size, self.l_size)
-        self.batch_norm2 = nn.BatchNorm1d(self.l_size)
-
-    def forward(self, x):
-
-        y = self.w1(x)
-        y = self.batch_norm1(y)
-        y = self.relu(y)
-        y = self.dropout(y)
-
-        y = self.w2(y)
-        y = self.batch_norm2(y)
-        y = self.relu(y)
-        y = self.dropout(y)
-
-        out = x + y
-
-        return out
+import math
+torch.autograd.set_detect_anomaly(True)  # Enable anomaly detection
 
 
 class MonolocoModel(nn.Module):
@@ -174,3 +77,139 @@ class MyLinear(nn.Module):
         out = x + y
 
         return out
+
+class LocoModel(nn.Module):
+    def __init__(self, input_size, output_size=2, linear_size=512, p_dropout=0.2, num_stage=3,device='cuda'):
+        super().__init__()
+
+        self.num_stage = num_stage
+        self.stereo_size = input_size
+        self.mono_size = int(input_size / 2)
+        self.output_size = output_size - 1
+        self.linear_size = linear_size
+        self.p_dropout = p_dropout
+        self.num_stage = num_stage
+        self.linear_stages = []
+        self.device = device
+
+        # Preprocessing
+        self.w1 = nn.Linear(self.stereo_size, self.linear_size)
+        self.batch_norm1 = nn.BatchNorm1d(self.linear_size)
+        self.relu = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout(self.p_dropout)
+
+        # Transformer layer
+        self.transformer = TransformerModel(self.stereo_size, self.linear_size, num_layers=5, num_heads=34, dropout=self.p_dropout)
+
+        # Internal loop
+        for _ in range(num_stage):
+            self.linear_stages.append(MyLinearSimple(self.linear_size, self.p_dropout))
+        self.linear_stages = nn.ModuleList(self.linear_stages)
+
+        # Post processing
+        self.w2 = nn.Linear(self.linear_size, self.linear_size)
+        self.w3 = nn.Linear(self.linear_size, self.linear_size)
+        self.batch_norm3 = nn.BatchNorm1d(self.linear_size)
+
+        # Auxiliary task
+        self.w_aux = nn.Linear(self.linear_size, 1)
+
+        # Final layers
+        self.w_fin = nn.Linear(self.linear_size, self.output_size)
+
+    def forward(self, x):
+        y = self.transformer(x)  # Pass y as both source and target
+        y = self.w1(y)
+        y = self.batch_norm1(y)
+        y = self.relu(y)
+        y = self.dropout(y)
+
+        # Transformer layer
+
+        for i in range(self.num_stage):
+            y = self.linear_stages[i](y)
+
+        # Auxiliary task
+        aux = self.w_aux(y)
+
+        # Final layers
+        y = self.w2(y)
+        y = self.w3(y)
+        y = self.batch_norm3(y)
+        y = self.relu(y)
+        y = self.dropout(y)
+        y = self.w_fin(y)
+
+        # Cat with auxiliary task
+        y = torch.cat((y, aux), dim=1)
+        return y
+
+
+class MyLinearSimple(nn.Module):
+    def __init__(self, linear_size, p_dropout=0.5):
+        super().__init__()
+        self.l_size = linear_size
+        self.relu = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout(p_dropout)
+
+        self.w1 = nn.Linear(self.l_size, self.l_size)
+        self.batch_norm1 = nn.BatchNorm1d(self.l_size)
+
+        self.w2 = nn.Linear(self.l_size, self.l_size)
+        self.batch_norm2 = nn.BatchNorm1d(self.l_size)
+
+    def forward(self, x):
+        y = self.w1(x)
+        y = self.batch_norm1(y)
+        y = self.relu(y)
+        y = self.dropout(y)
+
+        y = self.w2(y)
+        y = self.batch_norm2(y)
+        y = self.relu(y)
+        y = self.dropout(y)
+
+        out = x + y
+        return out
+
+
+class TransformerModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_heads, dropout):
+        super(TransformerModel, self).__init__()
+
+        # Positional encoding
+        self.positional_encoding = PositionalEncoding(input_size)
+
+        # Transformer
+        self.transformer = nn.Transformer(
+            d_model=input_size, nhead=num_heads, num_encoder_layers=num_layers,
+            num_decoder_layers=num_layers, dim_feedforward=hidden_size, dropout=dropout
+        )
+
+    def forward(self, x):
+        x = self.positional_encoding(x)
+        x = x.permute(1, 0, 2)
+        output = self.transformer(x, x)
+        output = output.permute(1, 0, 2)
+        output = output.squeeze()
+        return output
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, input_size, max_length=1594):
+        super(PositionalEncoding, self).__init__()
+        position = torch.arange(0, max_length).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, input_size, 2) * (-math.log(10000.0) / input_size))
+        pe = torch.zeros(max_length, input_size)
+        pe[:, 0::2] = torch.sin(position * div_term[:input_size // 2])
+        pe[:, 1::2] = torch.cos(position * div_term[:input_size // 2])
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        seq_length, input_size = x.size()
+        pe = self.pe[:, :seq_length, :input_size]
+        x = x + pe
+        return x
+    
+
